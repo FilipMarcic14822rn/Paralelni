c66bf7857d43feac9224e709f033cf2fccf33eb1
filip <fmarcic14822rn@raf.rs>
2022-12-13 11:12:45 +0100

    2.1

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ def draw():
 
 
 def avg_func(ans, tmp):
-    global avg_val, cnt, cols
+    global avg_val, cnt, rows, idx
 
     cnt += 1
 
     if cnt >= rows:
-        avg_val[tmp[0]] = ans[2] / rows
+        avg_val[tmp[idx]] = ans[2] / rows
         ans = (None, None, 0)
         cnt = 0
 
@@ -41,21 +41,20 @@ def avg_func(ans, tmp):
 
 
 def var_func(ans, tmp):
-    global avg_val, cnt, cols, i, flag
+    global var_val, cnt, rows, i, flag, idx
 
     cnt += 1
 
     if cnt >= rows:
-        var_val[f"cell_{i}"] = ans / (rows - 1)
-        ans = 0
+        var_val[tmp[idx]] = ans[2] / (rows - 1)
+        ans = None, None, 0
         cnt = 0
-        i += 1
 
     if flag:
         flag = False
-        return ans ** 2 + tmp ** 2
+        return None, None, ans[2] ** 2 + tmp[2] ** 2
 
-    return ans + tmp ** 2
+    return None, None, ans[2] + tmp[2] ** 2
 
 
 def standardize_func(val):
@@ -67,25 +66,28 @@ def standardize_func(val):
 
     cnt += 1
 
-    return f"cell_{i}", val[1], (val[2] - avg_val[f"cell_{i}"])/standard_dev[i]
+    return val[0], val[1], (val[2] - avg_val[val[0]]) / standard_dev[i]
 
-#def twopointone:
+
+# def twopointone:
 
 #    for j in
 
-rows, cols, data = read_table()
 
+rows, cols, data = read_table()
+print(rows, cols)
 avg_val = {}
 var_val = {}
 cnt = 1
 flag = True
 i = 0
+idx = 0
 
 reduce(avg_func, data)
 
 cnt = 1
 
-centered_val = map(lambda a: a[2] - avg_val[a[0]], data)
+centered_val = map(lambda a: (a[0], a[1], a[2] - avg_val[a[0]]), data)
 
 reduce(var_func, centered_val)
 
@@ -103,16 +105,27 @@ temp = rows
 rows = cols
 temp_avg = avg_val
 temp_var = var_val
+avg_val = {}
+var_val = {}
 flag = True
+idx = 1
+cnt = 1
 
 reduce(avg_func, data_by_gene)
-centered_val_by_gene = list(map(lambda a: a[2] - avg_val[a[0]], data_by_gene))
+
+cnt = 1
+
+centered_val_by_gene = list(map(lambda a: (a[0], a[1], a[2] - avg_val[a[1]]), data_by_gene))
+
 reduce(var_func, centered_val_by_gene)
 
-avg_val_by_gene = avg_val
-var_val_by_gene = var_val
-avg_val = temp_avg
-var_val = temp_var
-rows = temp
+for k, v in var_val.items():
+    print(k, v)
 
-print(var_val_by_gene)
+# avg_val_by_gene = avg_val
+# var_val_by_gene = var_val
+# avg_val = temp_avg
+# var_val = temp_var
+# rows = temp
+
+
